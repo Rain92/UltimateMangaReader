caf6a78a8a7548b592c5a9ad0c0a4883e0c70201
Andreas S <balduran174@gmail.com>
2020-12-28 14:12:06 +0100

    Fix select all on gotodialog

--- widgets/gotodialog.cpp
+++ widgets/gotodialog.cpp
@@ -45,7 +45,9 @@ bool GotoDialog::eventFilter(QObject *obj, QEvent *event)
 {
     if (event->type() == QEvent::FocusIn)
     {
-        static_cast<QSpinBox *>(obj)->selectAll();
+        auto box = static_cast<QSpinBox *>(obj);
+        QTimer::singleShot(0, box, &QSpinBox::selectAll);
+
         return false;
     }
     else if (event->type() == QEvent::RequestSoftwareInputPanel)
